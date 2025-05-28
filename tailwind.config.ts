29f3cd482fabb4ec21f57e58f46c78bcbb8559ed
Fredrick Maina <fchege04@gmail.com>
2025-05-28 08:33:44 +0300

    Add config for tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: {
+            DEFAULT: '#6366F1', // indigo-500
+            dark: '#4F46E5', // indigo-600
+            light: '#E0E7FF' // indigo-100
+          },
+          accent: '#22C55E', // green-500
+          danger: '#EF4444', // red-500
+          success: '#10B981', // green-600
+          surface: '#FFFFFF',
+          muted: '#6B7280', // gray-500
+        },
+      },
+    },
+  };
+  
